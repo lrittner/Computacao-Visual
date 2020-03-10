118574a03a41aacf2b8765d5e4647347d506cdcc
voltoline <raphavoltoline.rvr@gmail.com>
2020-03-10 17:42:08 -0300

    update lib path

--- src/__init__.py
+++ src/__init__.py
@@ -2,50 +2,50 @@
 # Toolbox ia898 
 #
 
-__version__ = 'ia898 Python Toolbox for Teaching Image  Processing version 0.1 12Mar2017'
-from ia898.src.affine import affine
-from ia898.src.applylut import applylut
-from ia898.src.bwlp import bwlp
-from ia898.src.colormap import colormap
-from ia898.src.conv import conv
-from ia898.src.comb import comb
-from ia898.src.cos import cos
-from ia898.src.circle import circle
-from ia898.src.dctmatrix import dctmatrix
-from ia898.src.dct import dct
-from ia898.src.dft import dft
-from ia898.src.dftmatrix import dftmatrix
-from ia898.src.dftshift import dftshift
-from ia898.src.dftview import dftview
-from ia898.src.ellipse import ellipse
-from ia898.src.gaussian import gaussian
-from ia898.src.gshow import gshow
-from ia898.src.haarmatrix import haarmatrix
-from ia898.src.hadamardmatrix import hadamardmatrix
-from ia898.src.hadamard import hadamard
-from ia898.src.histogram import histogram, histogram_eq, histogram_eq1
-from ia898.src.h2percentile import h2percentile
-from ia898.src.h2stats import h2stats
-from ia898.src.idct import idct
-from ia898.src.idft import idft
-from ia898.src.ihadamard import ihadamard
-from ia898.src.interpollin import interpollin
-from ia898.src.isccsym import isccsym,isccsym2
-from ia898.src.isolines import isolines
-from ia898.src.log import log
-from ia898.src.logfilter import logfilter
-from ia898.src.mosaic import mosaic
-from ia898.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
-from ia898.src.normalize import normalize
-from ia898.src.pca import pca
-from ia898.src.pconv import pconv
-from ia898.src.phasecorr import phasecorr
-from ia898.src.ptrans import ptrans, ptrans2, ptrans2d
-from ia898.src.polar import polar
-from ia898.src.ramp import ramp
-from ia898.src.rectangle import rectangle
-from ia898.src.sat import sat, satarea
-from ia898.src.sobel import sobel
+__version__ = 'ea979 Python Toolbox for Teaching Image  Processing version 0.1 10Mar2020'
+from ea979.src.affine import affine
+from ea979.src.applylut import applylut
+from ea979.src.bwlp import bwlp
+from ea979.src.colormap import colormap
+from ea979.src.conv import conv
+from ea979.src.comb import comb
+from ea979.src.cos import cos
+from ea979.src.circle import circle
+from ea979.src.dctmatrix import dctmatrix
+from ea979.src.dct import dct
+from ea979.src.dft import dft
+from ea979.src.dftmatrix import dftmatrix
+from ea979.src.dftshift import dftshift
+from ea979.src.dftview import dftview
+from ea979.src.ellipse import ellipse
+from ea979.src.gaussian import gaussian
+from ea979.src.gshow import gshow
+from ea979.src.haarmatrix import haarmatrix
+from ea979.src.hadamardmatrix import hadamardmatrix
+from ea979.src.hadamard import hadamard
+from ea979.src.histogram import histogram, histogram_eq, histogram_eq1
+from ea979.src.h2percentile import h2percentile
+from ea979.src.h2stats import h2stats
+from ea979.src.idct import idct
+from ea979.src.idft import idft
+from ea979.src.ihadamard import ihadamard
+from ea979.src.interpollin import interpollin
+from ea979.src.isccsym import isccsym,isccsym2
+from ea979.src.isolines import isolines
+from ea979.src.log import log
+from ea979.src.logfilter import logfilter
+from ea979.src.mosaic import mosaic
+from ea979.src.nbpil import adshow, show, nbshow, nbread, nbreadgray, nbwrite
+from ea979.src.normalize import normalize
+from ea979.src.pca import pca
+from ea979.src.pconv import pconv
+from ea979.src.phasecorr import phasecorr
+from ea979.src.ptrans import ptrans, ptrans2, ptrans2d
+from ea979.src.polar import polar
+from ea979.src.ramp import ramp
+from ea979.src.rectangle import rectangle
+from ea979.src.sat import sat, satarea
+from ea979.src.sobel import sobel
 
 
 
