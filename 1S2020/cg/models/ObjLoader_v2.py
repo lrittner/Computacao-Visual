b4e6eb9495adb1d45cc550d2544dac88e91b9788
voltoline <raphavoltoline.rvr@gmail.com>
2020-07-15 15:25:32 -0300

    Update ObjLoader_v2.py

--- 1S2020/cg/models/ObjLoader_v2.py
+++ 1S2020/cg/models/ObjLoader_v2.py
@@ -19,12 +19,17 @@ class ObjLoader:
     
         for name, material in self.scene.materials.items():
             
+            if(len(material.vertices) == 0):
+                print('material_name =', name, '- No vertice data')
+                continue
+            
             print('material_name =', name, '- vertex_format =', material.vertex_format, '- min =', min(material.vertices), '- max =', max(material.vertices))
             
             self.__objectInfo[name] = ObjectInfo()
             self.__objectInfo[name].numVertices = int(len(material.vertices) / material.vertex_size)
             self.__objectInfo[name].vertexFormat = material.vertex_format
             self.__objectInfo[name].vertices = np.empty(self.__objectInfo[name].numVertices * 4, dtype=np.float32)
+            self.__objectInfo[name].colors = []
             self.__objectInfo[name].normals = []
             self.__objectInfo[name].textureCoords = []
             
@@ -69,7 +74,24 @@ class ObjLoader:
                 self.__objectInfo[name].normals[0::3] = vertices[0::6]
                 self.__objectInfo[name].normals[1::3] = vertices[1::6]
                 self.__objectInfo[name].normals[2::3] = vertices[2::6]
+            
+            elif(self.__objectInfo[name].vertexFormat == "T2F_C3F_V3F"):
                 
+                vertices = np.array(material.vertices, dtype=np.float32)
+                self.__objectInfo[name].vertices[0::4] = vertices[5::8]
+                self.__objectInfo[name].vertices[1::4] = vertices[6::8]
+                self.__objectInfo[name].vertices[2::4] = vertices[7::8]
+                self.__objectInfo[name].vertices[3::4] = 1.0
+                
+                self.__objectInfo[name].colors = np.zeros(self.__objectInfo[name].numVertices * 3, dtype=np.float32)
+                self.__objectInfo[name].colors[0::3] = vertices[2::8]
+                self.__objectInfo[name].colors[1::3] = vertices[3::8]
+                self.__objectInfo[name].colors[2::3] = vertices[4::8]
+                
+                self.__objectInfo[name].textureCoords = np.zeros(self.__objectInfo[name].numVertices * 2, dtype=np.float32)
+                self.__objectInfo[name].textureCoords[0::2] = vertices[0::8]
+                self.__objectInfo[name].textureCoords[1::2] = vertices[1::8]
+                  
     def getItemNames(self):
         
         return list(self.__objectInfo)
@@ -77,6 +99,10 @@ class ObjLoader:
     def getVertexPositions(self, item_name):
     
         return self.__objectInfo[item_name].vertices
+
+    def getVertexColors(self, item_name):
+    
+        return self.__objectInfo[item_name].colors
 
     def getVertexTextureCoord(self, item_name):
         
@@ -89,6 +115,10 @@ class ObjLoader:
     def hasVertexNormals(self, item_name):
         
         return (len(self.__objectInfo[item_name].normals) > 0)
+
+    def hasVertexColors(self, item_name):
+        
+        return (len(self.__objectInfo[item_name].colors) > 0)
     
     def hasVertexTextureCoord(self, item_name):
         
